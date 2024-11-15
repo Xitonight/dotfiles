987634fc9ba21548fc60384a99fe1ab6cce51612
xitonight <xitonight@gmail.com>
2024-11-15 19:56:57 +0100

    added a lot of stuff again, mainly a nvim config

--- nvim/.config/nvim/init.lua
+++ nvim/.config/nvim/init.lua
@@ -1,6 +1,9 @@
-require("config.lazy")
-
+vim.g.mapleader = " "
 vim.cmd("set expandtab")
 vim.cmd("set tabstop=4")
 vim.cmd("set softtabstop=4")
 vim.cmd("set shiftwidth=4")
+
+require("config.lazy")
+
+
--- nvim/.config/nvim/lua/plugins/catppuccin.lua
+++ nvim/.config/nvim/lua/plugins/catppuccin.lua
@@ -1,12 +1,10 @@
 return {
-    { 
+    {
         "catppuccin/nvim",
         name = "catppuccin",
         priority = 1000,
         config = function()
-            require("catppuccin").setup({
-                -- Optional: any Catppuccin settings
-            })
+            require("catppuccin").setup({})
             vim.cmd.colorscheme "catppuccin"
         end,
     }
