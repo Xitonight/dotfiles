26acdbc4a661cb2c0477ff7c80083a5528b17a5d
xitonight <xitonight@gmail.com>
2024-11-16 14:21:10 +0100

    added linting and formatting for nvim

--- nvim/.config/nvim/init.lua
+++ nvim/.config/nvim/init.lua
@@ -4,6 +4,9 @@ vim.cmd("set tabstop=4")
 vim.cmd("set softtabstop=4")
 vim.cmd("set shiftwidth=4")
 
+vim.o.number = true          -- Enable absolute line numbers
+vim.o.relativenumber = true  -- Enable relative line numbers
+
 require("config.lazy")
 
 
--- nvim/.config/nvim/lua/plugins/lsp.lua
+++ nvim/.config/nvim/lua/plugins/lsp.lua
@@ -9,7 +9,7 @@ return {
         "williamboman/mason-lspconfig.nvim",
         config = function()
             require("mason-lspconfig").setup({
-                ensure_installed = { "lua_ls", "ts_ls" }
+                ensure_installed = { "lua_ls", "ts_ls", }
             })
         end
     },
@@ -21,7 +21,7 @@ return {
             lspconfig.ts_ls.setup({})
 
             vim.keymap.set('n', 'K', vim.lsp.buf.hover, {})
-            vim.keymap.set('n', 'gd', vim.lsp.buf.definition, {})
+            vim.keymap.set('n', '<leader>gd', vim.lsp.buf.definition, {})
             vim.keymap.set('n', '<leader>ca', vim.lsp.buf.code_action, {})
         end
     }
